39b06f93e4fa3f773e77587bd0d57b89652986ba
AMOGH DESHMUKH <71930347+AmoghDeshmukh@users.noreply.github.com>
2021-11-17 23:01:59 +0530

    Update OrderDetail.js

--- components/OrderDetail.js
+++ components/OrderDetail.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import PaypalBtn from './paypalBtn'
+import PaypalBtn from './PaypalBtn'
 import { patchData } from '../utils/fetchData'
 import { updateItem } from '../store/Actions'
 

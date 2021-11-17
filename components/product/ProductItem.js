70709eab8a488e61285a17f4f4afc077b8afdd0e
AMOGH DESHMUKH <71930347+AmoghDeshmukh@users.noreply.github.com>
2021-11-17 19:02:17 +0530

    Update ProductItem.js

--- components/product/ProductItem.js
+++ components/product/ProductItem.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { useContext } from 'react'
 import { DataContext } from '../../store/GlobalState'
 import { addToCart } from '../../store/Actions'
+import Image from 'next/image'
 
 
 const ProductItem = ({product}) => {
@@ -28,7 +29,7 @@ const ProductItem = ({product}) => {
     
     return(
         <div className="card" style={{ width: '18rem' }}>
-        <img className="card-img-top" src={product.images[0].url} alt={product.images[0].url} />
+        <Image className="card-img-top" src={product.images[0].url} alt={product.images[0].url} />
         <div className="card-body">
             <h5 className="card-title text-capitalize" title={product.title}>
             {product.title}

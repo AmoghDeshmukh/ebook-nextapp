fe82a3099fc95826502d7a463980b45147a88f13
AMOGH DESHMUKH <71930347+AmoghDeshmukh@users.noreply.github.com>
2021-11-17 20:11:26 +0530

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -71,7 +71,7 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link href="/">
-            <a className="navbar-brand">AMOGH'S MART</a>
+            <a className="navbar-brand">AMOGH MART</a>
             </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>

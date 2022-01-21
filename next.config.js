5762eeb51686c253c53e422b5cf675aedbd19bed
AMOGH DESHMUKH <71930347+AmoghDeshmukh@users.noreply.github.com>
2022-01-21 23:53:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     "BASE_URL": "https://nextby-amogh.herokuapp.com",
     "MONGODB_URL": "mongodb+srv://amogh05:Amogh_2001@cluster0.duej2.mongodb.net/amogh_ecom?retryWrites=true&w=majority",
-    "ACCESS_TOKEN_SECRET": "amogh054103628.604389506",
+    "ACCESS_TOKEN_SECRET": "amogh054103628.60438950",
     "REFRESH_TOKEN_SECRET": "amogh05secret10849321rere745602435",
     "PAYPAL_CLIENT_ID": "ASeptve29K8k409KV8FP15i-Xh7KgbDU4ETYg2SK9ZepzUk2D-rhxBuZPZubdvYkCCwXDDbTW1bT9dLC",
     "CLOUD_UPDATE_PRESET": "amogh_mart",
